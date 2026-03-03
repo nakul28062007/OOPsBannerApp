cce6d8383b7c948c871b65301a0e317f7f6e64dd
Nakulvasudev <nakul.thanigai@gmail.com>
2026-03-03 14:26:19 +0530

    UC1: Print OOPs to console

---
+++ OOPsBannerApp.java
@@ -0,0 +1,11 @@
+/**
+*OOPS Banner App
+*Prints The text OOps
+*@author Nakulvasudev T
+*@version
+*/
+public class OOPsBannerApp{
+	public static void main(String[] agrs){
+	System.out.println("OOPS Banner APP");
+	}
+}

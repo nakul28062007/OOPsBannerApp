cf7e883a6cbb166f583569b2f5b68e85bf531eb4
Nakulvasudev T <nakul.thanigai@gmail.com>
2026-03-18 12:51:52 +0530

    UC4: use String and Array for OOPs Banner

--- OOPsBannerApp.java
+++ OOPsBannerApp.java
@@ -11,49 +11,13 @@ public class OOPsBannerApp {
 
         // Create String array with 7 banner lines
         String[] lines = new String[7];
-
-        lines[0] = String.join(" ",
-                "  *****  ",
-                "  *****  ",
-                " ********",
-                " ******* ");
-
-        lines[1] = String.join(" ",
-                " *     * ",
-                " *     * ",
-                " *      *",
-                " *      *");
-
-        lines[2] = String.join(" ",
-                " *     * ",
-                " *     * ",
-                " *      *",
-                " *       ");
-
-        lines[3] = String.join(" ",
-                " *     * ",
-                " *     * ",
-                " ********",
-                "  ****** ");
-
-        lines[4] = String.join(" ",
-                " *     * ",
-                " *     * ",
-                " *       ",
-                "       * ");
-
-        lines[5] = String.join(" ",
-                " *     * ",
-                " *     * ",
-                " *       ",
-                " *      *");
-
-        lines[6] = String.join(" ",
-                "  *****  ",
-                "  *****  ",
-                " *       ",
-                "  ****** ");
-
+        lines[0] = String.join(" ", "  *****  ", "  *****  ", " ********", " ******* ");
+        lines[1] = String.join(" ", " *     * ", " *     * ", " *      *", " *      *");
+        lines[2] = String.join(" ", " *     * ", " *     * ", " *      *", " *       ");
+        lines[3] = String.join(" ", " *     * ", " *     * ", " ********", "  ****** ");
+        lines[4] = String.join(" ", " *     * ", " *     * ", " *       ", "       * ");
+        lines[5] = String.join(" ", " *     * ", " *     * ", " *       ", " *      *");
+        lines[6] = String.join(" ", "  *****  ", "  *****  ", " *       ", "  ****** ");
         // Enhanced for-loop to print each line
         for (String line : lines) {
             System.out.println(line);

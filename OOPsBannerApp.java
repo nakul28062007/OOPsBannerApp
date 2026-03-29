582146f4c31cc4b686bce2387ef3137f00a84a92
Nakulvasudev T <nakul.thanigai@gmail.com>
2026-03-29 21:57:37 +0530

    Modified with Maps

--- OOPsBannerApp.java
+++ OOPsBannerApp.java
@@ -1,32 +1,30 @@
 /**
  * OOPSBannerApp UC7 - OOPSBannerApp UC5 - Using Inner static method to print banner
  * @author Nakulvasudev T
- * @version 7.0
+ * @version 8.0
  */
-
+import java.util.Map;
+import java.util.LinkedHashMap;
 public class OOPsBannerApp {
-        public static void main(String[] args) {
-        System.out.println("OOPS Banner Using Innner static method:");    
+    public static void main(String[] args) {
+        System.out.println("OOPS Banner Using Innner static method:");
         BannerPrinter.printBanner();
-        }
+    }
 
-        static class BannerPrinter {
-                public static void printBanner(){
+    static class BannerPrinter {
 
-        // Create String array with 7 banner lines
-        String[] lines = {
-        String.join(" ", "  *****  ", "  *****  ", " ********", " ******* "),
-        String.join(" ", " *     * ", " *     * ", " *      *", " *      *"),
-        String.join(" ", " *     * ", " *     * ", " *      *", " *       "),
-        String.join(" ", " *     * ", " *     * ", " ********", "  ****** "),
-        String.join(" ", " *     * ", " *     * ", " *       ", "       * "),
-        String.join(" ", " *     * ", " *     * ", " *       ", " *      *"),
-        String.join(" ", "  *****  ", "  *****  ", " *       ", "  ****** "),
-        };
-        // Enhanced for-loop to print each line
-        for (String line : lines) {
-            System.out.println(line);
+        public static void printBanner(){
+            Map<Integer, String> lines = new LinkedHashMap<>();
+            lines.put(1, "  *****    *****   ********  *******");
+            lines.put(2, " *     *  *     *  *      *  *");
+            lines.put(3, " *     *  *     *  *      *  * ");
+            lines.put(4, " *     *  *     *  ********  *******");
+            lines.put(5, " *     *  *     *  *               *");
+            lines.put(6, " *     *  *     *  *               *");
+            lines.put(7, "  *****    *****   *         *******");
+            for(String line : lines.values()){
+                System.out.println(line);
+            }
         }
     }
-}
 }
